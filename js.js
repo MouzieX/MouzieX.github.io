89b442f74f19e3136ef35026beb625c486c30295
MouzieX <140145748+MouzieX@users.noreply.github.com>
2023-07-21 16:18:33 +0800

    Update js.js
    Signed-off-by: MouzieX <140145748+MouzieX@users.noreply.github.com>

--- js.js
+++ js.js
@@ -6,23 +6,3 @@ function savecookie(cookiename,cookieValue,cookieDates){
     document.cookie = cookiename+"="+cookieValue+";expires="+d.toUTCString();
 }
 
-document.onkeydown = function () {
-	if (
-		(event.ctrlKey && window.event.keyCode == 70) ||
-		(event.ctrlKey && window.event.keyCode == 71) ||
-		(event.ctrlKey && window.event.keyCode == 79) ||
-		(event.ctrlKey && window.event.keyCode == 80) ||
-		(event.ctrlKey && window.event.keyCode == 82) ||
-		(event.ctrlKey && window.event.keyCode == 83) ||
-		(event.ctrlKey && window.event.keyCode == 85) ||
-		(event.keyCode == 112) ||
-		(event.keyCode == 113) ||
-		(event.keyCode == 114) ||
-		(event.keyCode == 115) ||
-		(event.keyCode == 116) ||
-		(event.keyCode == 117) ||
-		(event.keyCode == 118) ||
-		(event.keyCode == 119) ||
-		(event.keyCode == 120) ||
-		(event.keyCode == 121)) { return false }
-};
