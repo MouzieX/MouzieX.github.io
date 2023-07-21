02fcdeec43ea7c60720522e8f336aee49e74894a
MouzieX <140145748+MouzieX@users.noreply.github.com>
2023-07-21 16:35:02 +0800

    Update js.js
    Signed-off-by: MouzieX <140145748+MouzieX@users.noreply.github.com>

--- js.js
+++ js.js
@@ -1,8 +1,2 @@
-console.log("%c Start Page \n Data:2023-04-10","color:#0072e2");
-
-function savecookie(cookiename,cookieValue,cookieDates){
-    var d=new Date();
-    d.setDate(d.getDate()+cookieDates)
-    document.cookie = cookiename+"="+cookieValue+";expires="+d.toUTCString();
-}
+console.log("%c Start Page \n Build:2023-04-10","color:#0072e2");
 
